7a98e4e20f066ef45990a14e7bd89be8a76a61e3
Alexis Brodeur <brodeuralexis@gmail.com>
2020-08-02 00:38:33 -0400

    Initial Commit

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A controller to handle looking around in a first person camera.
+///
+/// This controller expects the <see cref="playerObject"/> to be the container of the <see cref="playerCamera"/>.
+/// When looking around with a mouse, the <see cref="playerObject"/> will follow the yaw of the player, and the
+/// <see cref="playerCamera"/> will follow the pitch of the player.
+/// </summary>
+[AddComponentMenu("Rogue/First Person Controller")]
+public class FirstPersonController : MonoBehaviour
+{
+    /// <summary>
+    /// The <see cref="GameObject"/> that contains the player.
+    /// </summary>
+    public GameObject playerObject;
+
+    /// <summary>
+    /// The <see cref="Camera"/> that the player views the world through.
+    /// </summary>
+    public Camera playerCamera;
+
+    /// <summary>
+    /// The mouse sensitivity on the Y axis.
+    /// </summary>
+    public float verticalMouseSensitivity = 100f;
+
+    /// <summary>
+    /// The mouse sensitivity on the X axis.
+    /// </summary>
+    public float horizontalMouseSensitivity = 100f;
+
+    /// <summary>
+    /// Indicates if the vertical mouse movement should be inverted.
+    /// </summary>
+    public bool invertVerticalMouseMovement = false;
+
+    /// <summary>
+    /// The maximum vertical angle at which the player can look at.
+    /// </summary>
+    [Range(-90f, 90f)]
+    public float maximumVerticalMouseAngle = 90f;
+
+    /// <summary>
+    /// The minimum vertical angle at which the player can look at.
+    /// </summary>
+    [Range(-90f, 90f)]
+    public float minimumVerticalMouseAngle = -90f;
+
+    private CursorLockMode _previousCursorLockMode;
+    
+    private float _xRotation;
+
+    private void Start()
+    {
+        _xRotation = (maximumVerticalMouseAngle - minimumVerticalMouseAngle) / 2 + minimumVerticalMouseAngle;
+    }
+    
+    private void OnEnable()
+    {
+        _previousCursorLockMode = Cursor.lockState;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnDisable()
+    {
+        Cursor.lockState = _previousCursorLockMode;
+    }
+
+    private void Update()
+    {
+        var xMouseDelta = Input.GetAxis("Mouse X") * horizontalMouseSensitivity;
+        var yMouseDelta = Input.GetAxis("Mouse Y") * verticalMouseSensitivity;
+
+        _xRotation -= (invertVerticalMouseMovement ? -yMouseDelta : yMouseDelta) * Time.deltaTime;
+        _xRotation = Mathf.Clamp(_xRotation, minimumVerticalMouseAngle, maximumVerticalMouseAngle);
+        
+        playerCamera.transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        playerObject.transform.Rotate(Vector3.up, xMouseDelta * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A controller to handle basic movement input for a player.
+/// </summary>
+[AddComponentMenu("Rogue/Movement Controller")]
+public class MovementController : MonoBehaviour
+{
+    /// <summary>
+    /// The <see cref="GameObject"/> that contains the <see cref="CharacterController"/> of the player.
+    /// </summary>
+    public CharacterController playerCharacterController;
+    
+    /// <summary>
+    /// The movement speed of the player.
+    /// </summary>
+    public float movementSpeed = 12f;
+
+    /// <summary>
+    /// The vertical gravity that is to be applied to the player.
+    /// </summary>
+    public float gravity = -9.81f;
+
+    /// <summary>
+    /// The distance from the ground at which we considered to be on the ground.
+    /// </summary>
+    public float groundCheckDistance = 0.4f;
+
+    /// <summary>
+    /// The layer mask to which the ground check should apply.
+    /// </summary>
+    public LayerMask groundCheckMask;
+    
+    private Vector3 _velocity;
+    
+    private bool _grounded;
+    
+    private void Update()
+    {
+        var playerTransform = transform;
+        var playerTransformUp = playerTransform.up;
+        var forwardMovementDelta = Input.GetAxis("Vertical") * movementSpeed;
+        var sidewaysMovementDelta = Input.GetAxis("Horizontal") * movementSpeed;
+        
+        _grounded = Physics.CheckSphere(playerTransform.position, groundCheckDistance, groundCheckMask);
+
+        var upVelocity = Vector3.Dot(playerTransformUp, _velocity) / playerTransformUp.magnitude;
+        
+        if (_grounded && upVelocity < 0)
+        {
+            _velocity = Vector3.zero;
+        }
+
+        _velocity += playerTransform.forward * forwardMovementDelta +
+                     playerTransform.right * sidewaysMovementDelta +
+                     playerTransformUp * (gravity * Time.deltaTime);
+
+        playerCharacterController.Move(_velocity * Time.deltaTime);
+
+        // Only keep the upward/downward velocity.
+        _velocity = Vector3.Project(_velocity, playerTransformUp);
+    }
+}
